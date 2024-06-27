ea0bba21361d40f4ea828bd4cdb33ed3423f914d
Finn Fancher <fancherfinn@gmail.com>
2024-06-27 12:14:43 -0700

    update main.py

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    if(num1 < num2 and num2 < num3):
+        minval = num1
+        if(num2 < num3):
+            median = num2
+            maxval = num3
+        else:
+            median = num3
+            maxval = num2
+            
 
     print(minval, median, maxval)
     ########################################
